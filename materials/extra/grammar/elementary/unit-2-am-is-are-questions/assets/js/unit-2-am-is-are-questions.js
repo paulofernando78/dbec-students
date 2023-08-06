9d28ee158801c1105f7899661e4151e93f80dece
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-08-06 14:00:28 -0300

    up

---
+++ materials/extra/grammar/elementary/unit-2-am-is-are-questions/assets/js/unit-2-am-is-are-questions.js
@@ -0,0 +1,319 @@
+// EXERCISE 2.1
+// DROPDOWN
+function checkDropdowns() {
+  var answers = {
+    option1: "g",
+    option2: "f",
+    option3: "h",
+    option4: "c",
+    option5: "a",
+    option6: "e",
+    option7: "b",
+    option8: "i",
+    option9: "d",
+  };
+
+  var result = document.getElementById("result-dropdown");
+  var dropdownIcons = document.getElementsByClassName("dropdown-icon");
+
+  var correctCount = 0;
+
+  // Check each dropdown answer
+  for (var key in answers) {
+    if (answers.hasOwnProperty(key)) {
+      var selectedValue = document.getElementById(key).value;
+
+      if (selectedValue === answers[key]) {
+        correctCount++;
+        var icon = document.getElementById(key + "Icon");
+        icon.className = "dropdown-icon drop correct";
+      } else {
+        var icon = document.getElementById(key + "Icon");
+        icon.className = "dropdown-icon drop incorrect";
+      }
+
+      icon.style.display = "inline-block";
+    }
+  }
+
+  result.innerHTML =
+    "You got " +
+    correctCount +
+    " out of " +
+    Object.keys(answers).length +
+    " correct.";
+}
+function resetDropdowns() {
+  var dropdowns = document.getElementsByTagName("select");
+  var dropdownIcons = document.getElementsByClassName("dropdown-icon");
+
+  for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].selectedIndex = 0;
+  }
+
+  for (var j = 0; j < dropdownIcons.length; j++) {
+    dropdownIcons[j].className = "dropdown-icon";
+    dropdownIcons[j].style.display = "none";
+  }
+
+  document.getElementById("result-dropdown").innerHTML = "";
+}
+
+// EXERCISE 2.2
+// FILL-IN-THE-BLANKS 1
+function checkFill1() {
+  var answers = {
+    blank1_fill1: "How are",
+    blank2_fill1: ["Where is", "Where's", "Where is, Where's"],
+    blank3_fill1: "How old are",
+    blank4_fill1: "How much are",
+    blank5_fill1: ["What is", "What's", "What is, What's"],
+    blank6_fill1: ["Who is", "Who's", "Who is, Who's"],
+    blank7_fill1: [
+      "What color are",
+      "What colour are",
+      "What color are, What colour are",
+    ],
+  };
+
+  var fill1Icon1 = document.getElementById("fill1Icon1");
+  var fill1Icon2 = document.getElementById("fill1Icon2");
+  var fill1Icon3 = document.getElementById("fill1Icon3");
+  var fill1Icon4 = document.getElementById("fill1Icon4");
+  var fill1Icon5 = document.getElementById("fill1Icon5");
+  var fill1Icon6 = document.getElementById("fill1Icon6");
+  var fill1Icon7 = document.getElementById("fill1Icon7");
+  var result = document.getElementById("resultFill1");
+
+  var isCorrect1 =
+    document.getElementById("blank1_fill1").value.trim() ===
+    answers.blank1_fill1;
+  var isCorrect2 = answers.blank2_fill1.includes(
+    document.getElementById("blank2_fill1").value.trim()
+  );
+  var isCorrect3 =
+    document.getElementById("blank3_fill1").value.trim() ===
+    answers.blank3_fill1;
+  var isCorrect4 =
+    document.getElementById("blank4_fill1").value.trim() ===
+    answers.blank4_fill1;
+  var isCorrect5 = answers.blank5_fill1.includes(
+    document.getElementById("blank5_fill1").value.trim()
+  );
+  var isCorrect6 = answers.blank6_fill1.includes(
+    document.getElementById("blank6_fill1").value.trim()
+  );
+  var isCorrect7 = answers.blank7_fill1.includes(
+    document.getElementById("blank7_fill1").value.trim()
+  );
+
+  fill1Icon1.className = isCorrect1
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+  fill1Icon2.className = isCorrect2
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+  fill1Icon3.className = isCorrect3
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+  fill1Icon4.className = isCorrect4
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+  fill1Icon5.className = isCorrect5
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+  fill1Icon6.className = isCorrect6
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+  fill1Icon7.className = isCorrect7
+    ? "fill1-icon correct"
+    : "fill1-icon incorrect";
+
+  fill1Icon1.style.display = "inline-block";
+  fill1Icon2.style.display = "inline-block";
+  fill1Icon3.style.display = "inline-block";
+  fill1Icon4.style.display = "inline-block";
+  fill1Icon5.style.display = "inline-block";
+  fill1Icon6.style.display = "inline-block";
+  fill1Icon7.style.display = "inline-block";
+
+  var correctCount = [
+    isCorrect1,
+    isCorrect2,
+    isCorrect3,
+    isCorrect4,
+    isCorrect5,
+    isCorrect6,
+    isCorrect7,
+  ].filter(function (x) {
+    return x === true;
+  }).length;
+
+  result.innerHTML =
+    "You got " +
+    correctCount +
+    " out of " +
+    Object.keys(answers).length +
+    " correct.";
+}
+function showFill1() {
+  var answerElements = document.getElementsByClassName("fill1_answer");
+
+  for (var i = 0; i < answerElements.length; i++) {
+    var answerElement = answerElements[i];
+    answerElement.style.display = "block"; // Show the answers
+  }
+}
+function resetFill1() {
+  var fill1Inputs = document.getElementsByClassName("fill1");
+  var fill1Icons = document.getElementsByClassName("fill1-icon");
+
+  for (var i = 0; i < fill1Inputs.length; i++) {
+    fill1Inputs[i].value = "";
+  }
+
+  for (var j = 0; j < fill1Icons.length; j++) {
+    fill1Icons[j].className = "fill1-icon";
+    fill1Icons[j].style.display = "none";
+  }
+
+  document.getElementById("resultFill1").innerHTML = "";
+}
+
+// EXERCISE 2.3
+// FILL-IN-THE-BLANKS 2
+function checkFill2() {
+  var answers = {
+    blank1_fill2: ["What is your name?", "What's your name?"],
+    blank2_fill2: "Are you Australian?",
+    blank3_fill2: "How old are you?",
+    blank4_fill2: "Are you a teacher?",
+    blank5_fill2: "Are you married?",
+    blank6_fill2: "Is your wife a lawyer?",
+    blank7_fill2: ["Where is she from", "Where's she from?"],
+    blank8_fill2: ["What is her name?", "What's her name?"],
+    blank9_fill2: "How old is she?",
+  };
+
+  var fill2Icon1 = document.getElementById("fill2Icon1");
+  var fill2Icon2 = document.getElementById("fill2Icon2");
+  var fill2Icon3 = document.getElementById("fill2Icon3");
+  var fill2Icon4 = document.getElementById("fill2Icon4");
+  var fill2Icon5 = document.getElementById("fill2Icon5");
+  var fill2Icon6 = document.getElementById("fill2Icon6");
+  var fill2Icon7 = document.getElementById("fill2Icon7");
+  var fill2Icon8 = document.getElementById("fill2Icon8");
+  var fill2Icon9 = document.getElementById("fill2Icon9");
+  var result = document.getElementById("resultFill2");
+
+  var isCorrect1 = answers.blank1_fill2.includes(
+    document.getElementById("blank1_fill2").value.trim()
+  );
+  2;
+  var isCorrect2 =
+    document.getElementById("blank2_fill2").value.trim() ===
+    answers.blank2_fill2;
+  var isCorrect3 =
+    document.getElementById("blank3_fill2").value.trim() ===
+    answers.blank3_fill2;
+  var isCorrect4 =
+    document.getElementById("blank4_fill2").value.trim() ===
+    answers.blank4_fill2;
+  var isCorrect5 =
+    document.getElementById("blank5_fill2").value.trim() ===
+    answers.blank5_fill2;
+  var isCorrect6 =
+    document.getElementById("blank6_fill2").value.trim() ===
+    answers.blank6_fill2;
+  var isCorrect7 = answers.blank7_fill2.includes(
+    document.getElementById("blank7_fill2").value.trim()
+  );
+  var isCorrect8 = answers.blank8_fill2.includes(
+    document.getElementById("blank8_fill2").value.trim()
+  );
+  var isCorrect9 =
+    document.getElementById("blank9_fill2").value.trim() ===
+    answers.blank9_fill2;
+
+  fill2Icon1.className = isCorrect1
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon2.className = isCorrect2
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon3.className = isCorrect3
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon4.className = isCorrect4
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon5.className = isCorrect5
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon6.className = isCorrect6
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon7.className = isCorrect7
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon8.className = isCorrect8
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+  fill2Icon9.className = isCorrect9
+    ? "fill2-icon correct"
+    : "fill2-icon incorrect";
+
+  fill2Icon1.style.display = "inline-block";
+  fill2Icon2.style.display = "inline-block";
+  fill2Icon3.style.display = "inline-block";
+  fill2Icon4.style.display = "inline-block";
+  fill2Icon5.style.display = "inline-block";
+  fill2Icon6.style.display = "inline-block";
+  fill2Icon7.style.display = "inline-block";
+  fill2Icon8.style.display = "inline-block";
+  fill2Icon9.style.display = "inline-block";
+
+  var correctCount = [
+    isCorrect1,
+    isCorrect2,
+    isCorrect3,
+    isCorrect4,
+    isCorrect5,
+    isCorrect6,
+    isCorrect7,
+    isCorrect8,
+    isCorrect9,
+  ].filter(function (x) {
+    return x === true;
+  }).length;
+
+  result.innerHTML =
+    "You got " +
+    correctCount +
+    " out of " +
+    Object.keys(answers).length +
+    " correct.";
+}
+function showFill2() {
+  var answerElements = document.getElementsByClassName("fill2_answer");
+
+  for (var i = 0; i < answerElements.length; i++) {
+    var answerElement = answerElements[i];
+    answerElement.style.display = "block"; // Show the answers
+  }
+}
+function resetFill2() {
+  var fill2Inputs = document.getElementsByClassName("fill2");
+  var fill2Icons = document.getElementsByClassName("fill2-icon");
+
+  for (var i = 0; i < fill2Inputs.length; i++) {
+    fill2Inputs[i].value = "";
+  }
+
+  for (var j = 0; j < fill2Icons.length; j++) {
+    fill2Icons[j].className = "fill2-icon";
+    fill2Icons[j].style.display = "none";
+  }
+
+  document.getElementById("resultFill2").innerHTML = "";
+}
