3261612d7cf10b423f061a929d2a936092b7eb71
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-08-25 02:29:40 -0300

    up

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function buttonMenu() {
+    if (links.style.display == "block") {links.style = "none"
+    } else {
+        links.style.display = "block"
+    }
+}
+
+function buttonTips() {
+  if (tips.style.display == "block") {tips.style = "none";
+  } else {
+    tips.style.display = "block";
+  }
+}
--- js/main.js
+++
@@ -1,7 +0,0 @@
-function buttonMenu() {
-    if (links.style.display == "block") {
-        links.style = "none"
-    } else {
-        links.style.display = "block"
-    }
-}
