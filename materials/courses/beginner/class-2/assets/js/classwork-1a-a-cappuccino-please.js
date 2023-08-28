1a4a40f64bff60598beef2105cd0b66a5a07309a
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-08-28 03:12:15 -0300

    up

--- materials/courses/beginner/class-2/assets/js/classwork-1a-a-cappuccino-please.js
+++ materials/courses/beginner/class-2/assets/js/classwork-1a-a-cappuccino-please.js
@@ -6,52 +6,46 @@ function resetRadios() {
   }
 }
 
+// EXERCISE XXX
 // CHECKBOXES 1
-function checkCheckboxes() {
-  var checkboxes = document.querySelectorAll(".checkbox");
-  var icons = document.querySelectorAll(".checkbox-icon");
+function checkCheckboxes1() {
+  var checkboxes = document.querySelectorAll(".checkbox1");
+  var correctIcons = document.querySelectorAll(".checkbox-correct-icon1");
+  var incorrectIcons = document.querySelectorAll(".checkbox-incorrect-icon1");
   var totalCheckboxes = checkboxes.length;
   var correctCount = 0;
 
   for (var i = 0; i < totalCheckboxes; i++) {
     var checkbox = checkboxes[i];
-    var icon = icons[i];
+    var correctIcon = correctIcons[i];
+    var incorrectIcon = incorrectIcons[i];
 
-    if (checkbox.checked && checkbox.getAttribute("data-correct") === "true") {
-      correctCount++;
-    }
-
-    if (checkbox.getAttribute("data-correct") === "true") {
-      icon.classList.remove("invisible");
+    if (checkbox.checked) {
+      if (checkbox.getAttribute("data-correct") === "true") {
+        correctCount++;
+        correctIcon.classList.remove("invisible1");
+        incorrectIcon.classList.add("invisible1");
+      } else {
+        correctIcon.classList.add("invisible1");
+        incorrectIcon.classList.remove("invisible1");
+      }
+    } else {
+      correctIcon.classList.add("invisible1");
+      incorrectIcon.classList.add("invisible1");
     }
   }
 
-  var resultElement = document.getElementById("resultCheckboxes");
+  var resultElement = document.getElementById("resultCheckboxes1");
   resultElement.textContent =
     "You have selected " +
     correctCount +
     " correct answer(s) out of " +
-    document.querySelectorAll(".checkbox[data-correct='true']").length +
+    document.querySelectorAll(".checkbox1[data-correct='true']").length +
     ".";
 }
-function resetCheckboxes() {
-  var checkboxes = document.querySelectorAll(".checkbox");
-  var icons = document.querySelectorAll(".checkbox-icon");
-
-  for (var i = 0; i < checkboxes.length; i++) {
-    var checkbox = checkboxes[i];
-    var icon = icons[i];
-
-    checkbox.checked = false;
-    icon.classList.add("invisible");
-  }
-
-  var resultElement = document.getElementById("resultCheckboxes");
-  resultElement.textContent = "";
-}
-function showCheckboxes() {
-  var checkboxes = document.querySelectorAll(".checkbox");
-  var icons = document.querySelectorAll(".checkbox-icon");
+function showCheckboxes1() {
+  var checkboxes = document.querySelectorAll(".checkbox1");
+  var icons = document.querySelectorAll(".checkbox-correct-icon1");
   var selectedLetters = [];
 
   for (var i = 0; i < checkboxes.length; i++) {
@@ -59,33 +53,60 @@ function showCheckboxes() {
     var icon = icons[i];
 
     if (checkbox.getAttribute("data-correct") === "true") {
-      icon.classList.remove("invisible");
+      icon.classList.remove("invisible1");
       selectedLetters.push(checkbox.nextElementSibling.textContent.charAt(0));
     }
   }
 
-  var resultElement = document.getElementById("resultCheckboxes");
+  var resultElement = document.getElementById("resultCheckboxes1");
   resultElement.textContent =
     "Correct answers: " + selectedLetters.join(", ") + ".";
 }
+function resetCheckboxes1() {
+  var checkboxes = document.querySelectorAll(".checkbox1");
+  var correctIcons = document.querySelectorAll(".checkbox-correct-icon1");
+  var incorrectIcons = document.querySelectorAll(".checkbox-incorrect-icon1");
 
+  for (var i = 0; i < checkboxes.length; i++) {
+    var checkbox = checkboxes[i];
+    var correctIcon = correctIcons[i];
+    var incorrectIcon = incorrectIcons[i];
+
+    checkbox.checked = false;
+    correctIcon.classList.add("invisible1");
+    incorrectIcon.classList.add("invisible1");
+  }
+
+  var resultElement = document.getElementById("resultCheckboxes1");
+  resultElement.textContent = "";
+}
+
+// EXERCISE XXX
 // CHECKBOXES 2
 function checkCheckboxes2() {
   var checkboxes = document.querySelectorAll(".checkbox2");
-  var icons = document.querySelectorAll(".checkbox-icon2");
+  var correctIcons = document.querySelectorAll(".checkbox-correct-icon2");
+  var incorrectIcons = document.querySelectorAll(".checkbox-incorrect-icon2");
   var totalCheckboxes = checkboxes.length;
   var correctCount = 0;
 
   for (var i = 0; i < totalCheckboxes; i++) {
     var checkbox = checkboxes[i];
-    var icon = icons[i];
+    var correctIcon = correctIcons[i];
+    var incorrectIcon = incorrectIcons[i];
 
-    if (checkbox.checked && checkbox.getAttribute("data-correct") === "true") {
-      correctCount++;
-    }
-
-    if (checkbox.getAttribute("data-correct") === "true") {
-      icon.classList.remove("invisible2");
+    if (checkbox.checked) {
+      if (checkbox.getAttribute("data-correct") === "true") {
+        correctCount++;
+        correctIcon.classList.remove("invisible2");
+        incorrectIcon.classList.add("invisible2");
+      } else {
+        correctIcon.classList.add("invisible2");
+        incorrectIcon.classList.remove("invisible2");
+      }
+    } else {
+      correctIcon.classList.add("invisible2");
+      incorrectIcon.classList.add("invisible2");
     }
   }
 
@@ -94,27 +115,12 @@ function checkCheckboxes2() {
     "You have selected " +
     correctCount +
     " correct answer(s) out of " +
-    document.querySelectorAll(".checkbox[data-correct='true']").length +
+    document.querySelectorAll(".checkbox1[data-correct='true']").length +
     ".";
-}
-function resetCheckboxes2() {
-  var checkboxes = document.querySelectorAll(".checkbox2");
-  var icons = document.querySelectorAll(".checkbox-icon2");
-
-  for (var i = 0; i < checkboxes.length; i++) {
-    var checkbox = checkboxes[i];
-    var icon = icons[i];
-
-    checkbox.checked = false;
-    icon.classList.add("invisible2");
-  }
-
-  var resultElement = document.getElementById("resultCheckboxes2");
-  resultElement.textContent = "";
 }
 function showCheckboxes2() {
   var checkboxes = document.querySelectorAll(".checkbox2");
-  var icons = document.querySelectorAll(".checkbox-icon2");
+  var icons = document.querySelectorAll(".checkbox-correct-icon2");
   var selectedLetters = [];
 
   for (var i = 0; i < checkboxes.length; i++) {
@@ -130,4 +136,22 @@ function showCheckboxes2() {
   var resultElement = document.getElementById("resultCheckboxes2");
   resultElement.textContent =
     "Correct answers: " + selectedLetters.join(", ") + ".";
+}
+function resetCheckboxes2() {
+  var checkboxes = document.querySelectorAll(".checkbox2");
+  var correctIcons = document.querySelectorAll(".checkbox-correct-icon2");
+  var incorrectIcons = document.querySelectorAll(".checkbox-incorrect-icon2");
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    var checkbox = checkboxes[i];
+    var correctIcon = correctIcons[i];
+    var incorrectIcon = incorrectIcons[i];
+
+    checkbox.checked = false;
+    correctIcon.classList.add("invisible2");
+    incorrectIcon.classList.add("invisible2");
+  }
+
+  var resultElement = document.getElementById("resultCheckboxes2");
+  resultElement.textContent = "";
 }
