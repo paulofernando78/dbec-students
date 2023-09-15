23b94a48b76e568ed00dadbfd5b435cb8b2b621f
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-09-15 03:51:33 -0300

    up

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -115,8 +115,13 @@ const ShuffleCard2 = document.querySelector(".shuffle-card2");
 const ShuffleCard3 = document.querySelector(".shuffle-card3");
 const ShuffleCard4 = document.querySelector(".shuffle-card4");
 const ShuffleCard5 = document.querySelector(".shuffle-card5");
+const ShuffleCard6 = document.querySelector(".shuffle-card6");
+const ShuffleCard7 = document.querySelector(".shuffle-card7");
+const ShuffleCard8 = document.querySelector(".shuffle-card8");
+const ShuffleCard9 = document.querySelector(".shuffle-card9");
+const ShuffleCard10 = document.querySelector(".shuffle-card10");
 
-let currentZIndex = 4; // Initialize the current z-index
+let currentZIndex = 10; // Initialize the current z-index
 
 ShuffleCard1.addEventListener("click", (e) => {
   // Increment the current z-index
@@ -159,6 +164,46 @@ ShuffleCard5.addEventListener("click", (e) => {
 
   ShuffleCard5.classList.toggle("shuffle-card-flipped5");
   ShuffleCard5.style.zIndex = currentZIndex;
+});
+
+ShuffleCard6.addEventListener("click", (e) => {
+  // Increment the current z-index
+  currentZIndex++;
+
+  ShuffleCard6.classList.toggle("shuffle-card-flipped6");
+  ShuffleCard6.style.zIndex = currentZIndex;
+});
+
+ShuffleCard7.addEventListener("click", (e) => {
+  // Increment the current z-index
+  currentZIndex++;
+
+  ShuffleCard7.classList.toggle("shuffle-card-flipped7");
+  ShuffleCard7.style.zIndex = currentZIndex;
+});
+
+ShuffleCard8.addEventListener("click", (e) => {
+  // Increment the current z-index
+  currentZIndex++;
+
+  ShuffleCard8.classList.toggle("shuffle-card-flipped8");
+  ShuffleCard8.style.zIndex = currentZIndex;
+});
+
+ShuffleCard9.addEventListener("click", (e) => {
+  // Increment the current z-index
+  currentZIndex++;
+
+  ShuffleCard9.classList.toggle("shuffle-card-flipped9");
+  ShuffleCard9.style.zIndex = currentZIndex;
+});
+
+ShuffleCard10.addEventListener("click", (e) => {
+  // Increment the current z-index
+  currentZIndex++;
+
+  ShuffleCard10.classList.toggle("shuffle-card-flipped10");
+  ShuffleCard10.style.zIndex = currentZIndex;
 });
 
 // EXERCISE XXX
