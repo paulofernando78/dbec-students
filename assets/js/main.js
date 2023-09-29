e2bfc54dcde984478a0f54ab8a2d92f83f530ae7
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-09-29 04:10:22 -0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,6 @@ function loadPage(url) {
     .then((htmlContent) => {
       // Set the fetched HTML content as the innerHTML of the main section.
       document.getElementById("content").innerHTML = htmlContent;
-
-      // Update the URL in the browser's address bar
-      history.pushState(null, null, url);
     })
     .catch((error) => {
       console.error("There was a problem fetching the HTML:", error);
@@ -21,7 +18,7 @@ function loadPage(url) {
 
 // Self-invoking function to load "home.html" when the site initiates
 (function () {
-  loadPage("home.html");
+    loadPage('home.html');
 })();
 
 // BUTTON DROPDOWN MENU
