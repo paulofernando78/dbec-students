1df5c74cc075edad91279613735df997d32bf4bb
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-09-30 06:26:44 -0300

    up

--- assets/js/router/router.js
+++ assets/js/router/router.js
@@ -15,14 +15,24 @@ const routes = {
   grammar_elementary_unit_1_am_is_are:
     "extra/grammar/elementary/unit_1_am_is_are/unit_1_am_is_are.html",
   grammar_elementary_unit_2_am_is_are_questions:
-    "extra/grammar/elementary/unit-2-am-is-are-questions/unit-2-am-is-are-questions.html",
+    "extra/grammar/elementary/unit_2_am_is_are_questions/unit_2_am_is_are_questions.html",
   grammar_elementary_unit_3_i_am_doing_present_continuous:
     "extra/grammar/elementary/unit_3_i_am_doing_present_continuous/unit_3_i_am_doing_present_continuous.html",
+  grammar_elementary_unit_4_are_you_doing_present_continuous_questions:
+    "extra/grammar/elementary/unit_4_are_you_doing_present_continuous_questions/unit_4_are_you_doing_present_continuous_questions.html",
+  grammar_elementary_unit_5_i_do_work_like_etc_simple_present:
+    "extra/grammar/elementary/unit_5_i_do_work_like_etc_simple_present/unit_5_i_do_work_like_etc_simple_present.html",
+  grammar_elementary_unit_6_i_dont_simple_present_negative:
+    "extra/grammar/elementary/unit_6_i_dont_simple_present_negative/unit_6_i_dont_simple_present_negative.html",
 
   vocabulary: "extra/vocabulary/vocabulary.html",
+  essential_words: "extra/vocabulary/essential_words/essential_words.html",
   false_cognates: "extra/vocabulary/(false)_cognates/(false)_cognates.html",
 
   listening: "extra/listening/listening.html",
+  penguin_kids: "extra/listening/penguin_kids/penguin_kids.html",
+  penguin_kids_level_1_dumbo: "extra/listening/penguin_kids/dumbo/dumbo.html",
+
   reading: "extra/reading/reading.html",
   speaking: "extra/speaking/speaking.html",
   pronunciation: "extra/pronunciation/pronunciation.html",
