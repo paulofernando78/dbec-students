7a2ba465c5e1dba945e4c78769cd8e6620e47cd4
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-10-01 01:51:01 -0300

    up

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require("express");
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) =>
+res.send("<h1>Server OK</h1>")
+);
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+console.log("Servidor iniciando na porta 3000");
 });
