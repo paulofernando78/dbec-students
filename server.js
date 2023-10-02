5847a2690ad313b328d5386fa241fcfafd5515d7
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-10-02 11:36:22 -0300

    up

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 const port = 3000;
 
 // Serve static files from a directory (e.g., public)
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
 app.listen(port, () => {
-console.log(`Server is at http://localhost:3000`);
+console.log(`Server is listening at http://localhost:3000`);
 });
