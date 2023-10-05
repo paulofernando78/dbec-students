dbeada200b4d553ee2fc1f73c34a5629c9b40fb3
Paulo Fernando <124406530+paulofernando78@users.noreply.github.com>
2023-10-05 19:04:36 -0300

    added js for hiding nav_bar after clicling on link

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,6 +8,23 @@ function buttonMenu() {
   }
 }
 
+function hideNavBar() {
+  const links = document.getElementById("nav_bar");
+  const screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  if (screenWidth <= 600) {
+    links.style.display = "none";
+  }
+}
+
+const navLinks = document.querySelectorAll("#nav_bar a");
+navLinks.forEach(function(link) {
+  link.addEventListener("click", hideNavBar);
+});
+
 // CLASSWORK PRACTICE COPY LINK
 
 // GRAMMAR
